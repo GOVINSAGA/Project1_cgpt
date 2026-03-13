a1ebfdcad88e4666197b79d0638da931540a145a
GOVINSAGA <73192061+GOVINSAGA@users.noreply.github.com>
2026-03-14 00:44:40 +0530

    done migration to make changes in db(increse password length)

---
+++ Project1_cgpt/Migrations/20260313190658_UpdatePasswordLength.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project1_cgpt.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePasswordLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
